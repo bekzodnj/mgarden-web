1238ba6da9b74b759f506b46ea62ef840d8f7b15
Bekzodjon <bekzodnx@gmail.com>
2025-05-17 12:09:07 +0500

    added new article

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sorolla';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Maveran Garden";
+export const SITE_DESCRIPTION =
+  "Welcome to my website! Our services specialized in transportation and good services";
